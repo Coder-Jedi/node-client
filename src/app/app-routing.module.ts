dea1263fe3a979521f31d7acf54cefee09156f25
Hritik Madankar <madankar.hritik@gmail.com>
2024-09-05 02:23:00 +0530

    maintenance page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./maintenance/maintenance.module').then(m => m.MaintenanceModule)
+  },
+  {
+    path: 'maintenance',
+    loadChildren: () => import('./maintenance/maintenance.module').then(m => m.MaintenanceModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
